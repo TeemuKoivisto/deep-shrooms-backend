a291aec7ba32893806276ac36eeac93608813890
Teemu Koivisto <tkoivisto456@gmail.com>
2017-10-25 16:27:15 +0300

    Create first working version

--- app.py
+++ app.py
@@ -3,58 +3,36 @@ from flask_cors import CORS
 
 from scipy.misc import imsave, imread, imresize
 import numpy as np
-# import keras.models
-# import re
+import keras.models
 
-import base64
-
-import sys, os, io
+import sys, os
 sys.path.append(os.path.abspath('./model'))
 
-# import load as load_model
-# global model, graph
-# model, graph = load_model.init()
+import load as load_model
+global model, graph
+model, graph = load_model.init()
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"*": {"origins": "*"}})
+
+def generate_input_image(img_array):
+	img_resized = imresize(img_array, (480, 480))
+	print(img_resized.shape)
+	imsave('resized.jpg', img_resized)
+	X = img_resized.reshape(1, 480, 480, 3)
+	X = X/255.0
+	return X
 
 @app.route('/predict', methods=['POST'])
 def predict():
 	formFile = request.files.get('file')
 	img = imread(formFile)
-	print(img.shape)
-	return jsonify({ "len": len(img) })
-
-def predict2():
-	# imgData = request.get_data()
-	imgData = request.form['img']
-	#encode it into a suitable format
-	convertImage(imgData)
-	print("debug")
-	#read the image into memory
-	x = imread('output.png',mode='L')
-	#make it the right size
-	x = imresize(x,(28,28))
-	#imshow(x)
-	#convert to a 4D tensor to feed into our model
-	x = x.reshape(1,28,28,1)
-	print("debug2")
-	#in our computation graph
+	X = generate_input_image(img)
 	with graph.as_default():
-		#perform the prediction
-		out = model.predict(x)
-		print(out)
-		print(np.argmax(out,axis=1))
-		print("debug3")
-		#convert the response to a string
-		response = np.array_str(np.argmax(out,axis=1))
-		return response
-
-def convertImage(imgData1):
-    imgstr = re.search(b'base64,(.*)',imgData1).group(1)
-    with open('output.png','wb') as output:
-        output.write(base64.b64decode(imgstr))
+		prediction = model.predict(X)
+		print(prediction)
+		pred_value = prediction.flatten().tolist()[0]
+		return jsonify({ "prediction": pred_value })
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=os.environ.get('PORT', 9000))
-	#app.run(debug=True)
