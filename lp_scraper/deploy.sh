4aa71a04f7dea7860661f370e7a46022524f8dba
Teemu Koivisto <tkoivisto456@gmail.com>
2017-10-30 13:31:07 +0200

    Add third argument to deploy.sh

--- lp_scraper/deploy.sh
+++ lp_scraper/deploy.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
-# Deploys output -folder as a zipped dataset to S3.
+# Deploys a folder as a zip-file to S3 bucket "deep-shrooms".
 
-NAME=$1
-PROFILE=$2
+TARGET_DIR=$1
+ZIP_NAME=$2
+PROFILE=$3
 
-if [ -z "$NAME" ]; then
-  echo "Missing first argument NAME for the deployed dataset."
+if [ -z "$ZIP_NAME" ]; then
+  echo "Missing first argument TARGET_DIR. It should be the target folder you're trying to deploy."
+  exit 0
+fi
+if [ -z "$ZIP_NAME" ]; then
+  echo "Missing second argument ZIP_NAME. It will be used as the name of the dataset."
   exit 0
 fi
 if [ -z "$PROFILE" ]; then
-  echo "Missing second argument PROFILE for the AWS profile to be used for deploying."
+  echo "Missing third argument PROFILE. It's the AWS profile you want to use for deploying."
   exit 0
 fi
 
-cp -r ./output ./$NAME
-zip -r ./$NAME.zip ./$NAME
-rm -r ./$NAME
+cp -r ./$TARGET_DIR ./$ZIP_NAME
+zip -r ./$ZIP_NAME.zip ./$ZIP_NAME
+rm -r ./$ZIP_NAME
 
-aws s3 cp ./$NAME.zip s3://deep-shrooms \
+aws s3 cp ./$ZIP_NAME.zip s3://deep-shrooms \
   --region eu-central-1 \
   --acl bucket-owner-full-control \
   --cache-control max-age=2592000 \
   --profile $PROFILE
 
-rm ./$NAME.zip
+rm ./$ZIP_NAME.zip
